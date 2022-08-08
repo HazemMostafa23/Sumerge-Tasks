f170835ec30f9ffb66f478f864cf752d9b22453f
HazemMostafa23 <85435202+HazemMostafa23@users.noreply.github.com>
2022-08-08 13:23:23 +0200

    Add files via upload

--- task2/logic.js
+++ task2/logic.js
@@ -35,7 +35,14 @@ function win(player) {
         btn_list.push(document.getElementById([i]).value);
     }
 
-    if (btn_list[0] == player && btn_list[1] == player && btn_list[2] == player) {
+    if (btn_list[0] == player && btn_list[1] == player && btn_list[2] == player ||btn_list[0] == player && btn_list[3] == player && btn_list[6] == player
+        || btn_list[0] == player && btn_list[4] == player && btn_list[8] == player||
+       
+        btn_list[1] == player && btn_list[4] == player && btn_list[7] == player||
+        btn_list[2] == player && btn_list[5] == player && btn_list[8] == player||
+        btn_list[3] == player && btn_list[4] == player && btn_list[5] == player||
+        btn_list[6] == player && btn_list[7] == player && btn_list[8] == player||
+        btn_list[6] == player && btn_list[4] == player && btn_list[2] == player) {
         if (player == "X") {
 
             document.getElementById('winner')
@@ -54,147 +61,7 @@ function win(player) {
         setTimeout(function () { alert("Player " + player + " Won"); }, 200);
 
     }
-    else if (btn_list[0] == player && btn_list[3] == player && btn_list[6] == player) {
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
-    else if (btn_list[0] == player && btn_list[4] == player && btn_list[8] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-    }
-    else if (btn_list[0] == player && btn_list[4] == player && btn_list[8] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
-    else if (btn_list[1] == player && btn_list[4] == player && btn_list[7] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-
-    }
-    else if (btn_list[2] == player && btn_list[5] == player && btn_list[8] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
-    else if (btn_list[3] == player && btn_list[4] == player && btn_list[5] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
-    else if (btn_list[6] == player && btn_list[7] == player && btn_list[8] == player) {
-
-        if (player == "X") {
-
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: green;'>X </span>" + " won";
-
-
-        }
-        else {
-            document.getElementById('winner')
-                .innerHTML = "Player " + "<span style='color: red;'>O </span>" + " won";
-
-
-        }
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
-    else if (btn_list[6] == player && btn_list[4] == player && btn_list[2] == player) {
-
-        document.getElementById('winner')
-            .innerHTML = "Player " + player + " won";
-        disablebtns();
-        setTimeout(function () { alert("Player " + player + " Won"); }, 200);
-
-    }
+   
     else if (((btn_list[0] == 'X' || btn_list[0] == 'O') && (btn_list[1] == 'X'
         || btn_list[1] == 'O') && (btn_list[2] == 'X' || btn_list[2] == 'O') &&
         (btn_list[3] == 'X' || btn_list[3] == 'O') && (btn_list[4] == 'X' ||
